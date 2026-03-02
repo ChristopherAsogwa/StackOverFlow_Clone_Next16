724b769b791e721cc63dbaa9b14e4235f4553643
ChristopherAsogwa <christopherasogwa8@gmail.com>
2026-03-02 05:41:06 +0100

    Implement Question Card

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true,
-    },
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
